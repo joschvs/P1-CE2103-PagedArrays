9b56f8ac785c727412d7f8042acbd3dfa36fe525
Jose Chaves Chacón <jmch0406@gmail.com>
2026-04-07 17:50:21 -0600

    feat: selectionSort

---
+++ sorter/SelectionSort.h
@@ -0,0 +1,26 @@
+#ifndef P1_CE2103_PAGEDARRAYS_SELECTIONSORT_H
+#define P1_CE2103_PAGEDARRAYS_SELECTIONSORT_H
+
+void selectionSort(PagedArray& arr, long n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+
+        if (min != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
+#endif
