3194d705ba5da633ecb8a81f66e239386712a1ce
Jose Chaves Chacón <jmch0406@gmail.com>
2026-04-07 18:45:49 -0600

    feat: heapSort

--- sorter/BubbleSort.h
+++ sorter/BubbleSort.h
@@ -1,19 +1,38 @@
 #ifndef P1_CE2103_PAGEDARRAYS_BUBBLESORT_H
 #define P1_CE2103_PAGEDARRAYS_BUBBLESORT_H
 
-void bubbleSort(PagedArray& arr, long n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
+void heapify(PagedArray& arr, long n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(PagedArray& arr, long n) {
+
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
     }
 }
 
