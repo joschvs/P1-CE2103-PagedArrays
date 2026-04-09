8fcafca5f799df20dbd34118b8f9a17a7b67de36
Jose Chaves Chacón <jmch0406@gmail.com>
2026-04-09 04:06:26 -0600

    feat: algoritmos copiados con diferente firma para el baseline

---
+++ baseline/BubbleSort.h
@@ -0,0 +1,20 @@
+#ifndef P1_CE2103_PAGEDARRAYS_BUBBLESORT_H
+#define P1_CE2103_PAGEDARRAYS_BUBBLESORT_H
+
+inline void bubbleSort(int* arr, long n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+#endif
---
+++ baseline/InsertionSort.h
@@ -0,0 +1,19 @@
+#ifndef P1_CE2103_PAGEDARRAYS_INSERTIONSORT_H
+#define P1_CE2103_PAGEDARRAYS_INSERTIONSORT_H
+
+inline void insertionSort(int* arr, long n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int elem = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > elem) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = elem;
+    }
+}
+
+#endif
