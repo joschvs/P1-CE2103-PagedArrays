9ab3365b6ebeaaba46512a8f91f464b1033cd03d
Jose Chaves Chacón <jmch0406@gmail.com>
2026-04-09 02:48:56 -0600

    fix: optimizar operator[] de PageArray

--- sorter/PagedArray.h
+++ sorter/PagedArray.h
@@ -14,15 +14,98 @@ private:
     int** pages;
     int* pageIDs;
     bool* modified;
-    int pageFaults;
-    int pageHits;
-    int* lastUsed;
+    long long pageFaults;
+    long long pageHits;
+    long long* lastUsed;
     int slot;
-    int useCounter;
+    long long useCounter;
+
+    int hashTableSize;
+    int* hashKeys;
+    int* hashValues;
+    bool* hashUsed;
+
+    int hashFunc(int key)
+    {
+        unsigned int k = (unsigned int)key;
+        return k % hashTableSize;
+    }
+
+    void hashInit(int capacity)
+    {
+        hashTableSize = capacity * 4 + 1;
+        hashKeys = new int[hashTableSize];
+        hashValues = new int[hashTableSize];
+        hashUsed = new bool[hashTableSize];
+        for (int i = 0; i < hashTableSize; i++)
+        {
+            hashUsed[i] = false;
+        }
+    }
+
+    void hashInsert(int pageID, int slot)
+    {
+        int idx = hashFunc(pageID);
+        while (hashUsed[idx] && hashKeys[idx] != pageID)
+        {
+            idx = (idx + 1) % hashTableSize;
+        }
+        hashKeys[idx] = pageID;
+        hashValues[idx] = slot;
+        hashUsed[idx] = true;
+    }
+
+    int hashFind(int pageID)
+    {
+        int idx = hashFunc(pageID);
+        int start = idx;
+        while (hashUsed[idx])
+        {
+            if (hashKeys[idx] == pageID)
+            {
+                return hashValues[idx];
+            }
+            idx = (idx + 1) % hashTableSize;
+            if (idx == start) break;
+        }
+        return -1;
+    }
+
+    void hashRemove(int pageID)
+    {
+        int idx = hashFunc(pageID);
+        int start = idx;
+        while (hashUsed[idx])
+        {
+            if (hashKeys[idx] == pageID)
+            {
+                hashUsed[idx] = false;
+                int next = (idx + 1) % hashTableSize;
+                while (hashUsed[next])
+                {
+                    int tmpKey = hashKeys[next];
+                    int tmpVal = hashValues[next];
+                    hashUsed[next] = false;
+                    hashInsert(tmpKey, tmpVal);
+                    next = (next + 1) % hashTableSize;
+                }
+                return;
+            }
+            idx = (idx + 1) % hashTableSize;
+            if (idx == start) break;
+        }
+    }
+
+    void hashFree()
+    {
+        delete[] hashKeys;
+        delete[] hashValues;
+        delete[] hashUsed;
+    }
 
     void writePage(int slot)
     {
-        long pos = pageIDs[slot] * pageSize * sizeof(int);
+        long pos = (long)pageIDs[slot] * pageSize * sizeof(int);
 
         fseek(file, pos, SEEK_SET);
         fwrite(pages[slot], sizeof(int), pageSize, file);
@@ -51,12 +134,17 @@ private:
 
     void loadPage(int LRUslot, int pageID)
     {
+        if (pageIDs[LRUslot] != -1)
+        {
+            hashRemove(pageIDs[LRUslot]);
+        }
+
         if (modified[LRUslot])
         {
             writePage(LRUslot);
         }
 
-        long pos = pageID * pageSize * sizeof(int);
+        long pos = (long)pageID * pageSize * sizeof(int);
 
         fseek(file, pos, SEEK_SET);
         fread(pages[LRUslot], sizeof(int), pageSize, file);
@@ -64,14 +152,68 @@ private:
         modified[LRUslot] = false;
         pageIDs[LRUslot] = pageID;
 
+        hashInsert(pageID, LRUslot);
     }
 
+    int resolveSlot(long index)
+    {
+        int page = index / pageSize;
+        int ramSlot = hashFind(page);
+
+        if (ramSlot != -1)
+        {
+            pageHits++;
+        }
+        else
+        {
+            pageFaults++;
+            int LRUslot = findLRUslot();
+            loadPage(LRUslot, page);
+            ramSlot = LRUslot;
+        }
+
+        lastUsed[ramSlot] = ++useCounter;
+        return ramSlot;
+    }
 
 public:
+    class Proxy
+    {
+    private:
+        PagedArray& arr;
+        long index;
+    public:
+        Proxy(PagedArray& a, long i) : arr(a), index(i) {}
+
+        operator int() const
+        {
+            int ramSlot = arr.resolveSlot(index);
+            int pageSlot = index % arr.pageSize;
+            return arr.pages[ramSlot][pageSlot];
+        }
+
+        Proxy& operator=(int value)
+        {
+            int ramSlot = arr.resolveSlot(index);
+            int pageSlot = index % arr.pageSize;
+            arr.pages[ramSlot][pageSlot] = value;
+            arr.modified[ramSlot] = true;
+            return *this;
+        }
+
+        Proxy& operator=(const Proxy& other)
+        {
+            int value = (int)other;
+            *this = value;
+            return *this;
+        }
+    };
+
     PagedArray(char* filePath, int pageSize, int pageCount)
     {
         pageFaults = 0;
         pageHits = 0;
+        useCounter = 0;
 
         file = fopen(filePath, "rb+");
 
@@ -79,15 +221,16 @@ public:
         this->pageCount = pageCount;
 
         modified = new bool[pageCount];
-        lastUsed = new int[pageCount];
+        lastUsed = new long long[pageCount];
         pages = new int*[pageCount];
         pageIDs = new int[pageCount];
-        useCounter = 0;
+
+        hashInit(pageCount);
 
         for (int i = 0; i < pageCount; i++)
         {
             modified[i] = false;
-            lastUsed[i] = 0;
+            lastUsed[i] = -1;
             pages[i] = new int[pageSize];
             pageIDs[i] = -1;
         }
@@ -114,44 +257,14 @@ public:
 
         delete[] pages;
 
+        hashFree();
+
         fclose(file);
     }
 
-    int& operator[](long index)
+    Proxy operator[](long index)
     {
-        int page = index / pageSize;
-        int pageSlot = index % pageSize;
-        bool pageFault = false;
-        int ramSlot = -1;
-
-        for (int i = 0; i < pageCount; i++)
-        {
-            if (pageIDs[i] == page)
-            {
-                pageHits += 1;
-                pageFault = false;
-                ramSlot = i;
-                break;
-
-            }
-            else
-            {
-                pageFault = true;
-            }
-        }
-
-        if (pageFault)
-        {
-            pageFaults += 1;
-            int LRUslot = findLRUslot();
-            loadPage(LRUslot, page);
-            ramSlot = LRUslot;
-        }
-
-        lastUsed[ramSlot] = ++useCounter;
-        modified[ramSlot] = true;
-
-        return pages[ramSlot][pageSlot];
+        return Proxy(*this, index);
     }
 
     void stats()
@@ -160,8 +273,5 @@ public:
         std::cout << "Page hits:" << pageHits << "\n";
     }
 };
-
-
-
 
 #endif //P1_CE2103_PAGEDARRAYS_MAIN_H
